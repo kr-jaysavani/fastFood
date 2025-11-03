91c833e43d66db1222adbd6ab442abaf4b0fb2ab
kr-jaysavani <jay.s@knackroot.com>
2025-11-03 15:10:27 +0530

    added button for navigation in auth page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,12 @@
+import { router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 const Signup = () => {
   return (
     <View>
       <Text>Signup</Text>
+      <Button title="SignIn" onPress={() => router.push("/sign-in")} />
     </View>
   );
 };
